70e56174514348b98d48a69333c2f7a193fabda8
Fernando Daciuk <f.daciuk@gmail.com>
2017-11-10 10:32:50 -0200

    Add component and test

---
+++ jest-with-react/src/react-component.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import React from 'react'
+
+const Component = () => (
+  <h1>Title</h1>
+)
+
+export default Component
---
+++ jest-with-react/src/react-component.test.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import React from 'react'
+import Component from './react-component'
+
+it('Test', () => {
+  console.log(<Component />)
+  console.log(<Component />.props)
+})
